b4416ace5a19169b5a8eb6be89fbe02b991674b9
begüm muşdal <begum_musdal@hotmail.com>
2021-01-20 22:20:38 +0300

    first commit

---
+++ CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void CustomerList(Customer[] customers)
+        {
+            Console.WriteLine("Customer List:");
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Id: {0}  Firstname: {1}  LAstname: {2}", customer.Id, customer.Firstname, customer.Lastname);
+            }
+        }
+        public void CustomerAdd(Customer customer)
+        {
+            Console.WriteLine("{0} {1} added!", customer.Firstname, customer.Lastname);
+        }
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine("{0} {1} deleted!", customer.Firstname, customer.Lastname);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Customer customer1 = new Customer()
+            Customer customer1 = new Customer
             {
                 Id = 1,
                 Firstname = "Begüm",
                 Lastname = "Muşdal"
             };
-            Customer customer2 = new Customer()
+            Customer customer2 = new Customer
             {
                 Id = 2,
                 Firstname = "Begüm",
@@ -26,33 +26,6 @@ namespace ClassMetotDemo
             customerManager.CustomerDelete(customer2);
 
             Console.ReadLine();
-        }
-        class Customer
-        {
-            public int Id { get; set; }
-            public string Firstname { get; set; }
-            public string Lastname { get; set; }
-        }
-        class CustomerManager
-        {
-            public void CustomerList(Customer[] customers)
-            {
-                Console.WriteLine("Customer List:");
-                foreach (var customer in customers)
-                {
-                    Console.WriteLine("Id: {0}  Firstname: {1}  LAstname: {2}", customer.Id, customer.Firstname, customer.Lastname);
-                }
-            }
-            public void CustomerAdd(Customer customer)
-            {
-                Console.WriteLine("{0} {1} added!", customer.Firstname, customer.Lastname);
-            }
-            public void CustomerDelete(Customer customer)
-            {
-                Console.WriteLine("{0} {1} deleted!", customer.Firstname, customer.Lastname);
-            }
-
-        }
-        
+        } 
     }
 }
